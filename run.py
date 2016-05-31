08c98a9fcefc2db0fbe18c209a21c9a13ca1a6b0
Brad Smith <brad.alan.smith@gmail.com>
2016-05-31 14:38:49 -0700

    update run

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 """Run the server in development mode."""
-from relately.server import app
+from relately.server import get_instance
 
 if __name__ == "__main__":
+    app = get_instance()
     app.secret_key = 'super secret key'
     app.run(
         host='0.0.0.0',
